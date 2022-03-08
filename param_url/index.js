5875c28b5a2df3f4af6a2e5c190d75f62f7e2409
Your Namerogeriodesaf <rogeriodesaf@gmail.com>
2022-03-08 09:25:48 -0300

    upgrade

--- param_url/index.js
+++ param_url/index.js
@@ -5,11 +5,12 @@ const porta   = 3000;
 
 const basePath = path.join(__dirname, 'template');
 
-app.get('/users/:id', (req,res)=>{
+app.get('/users/:id?', (req,res)=>{
     const id = req.params.id
 
     //FAZ A LEITURA NA TABELA USERS E RESGATA O USUÁRIO NO BANCO
-    console.log('Buscando o usuário',id)
+    console.log('Buscando o usuário',req.params.id)
+  
 
     res.sendFile(`${basePath}/users.html`)
 });
