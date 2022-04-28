acf0b290a6ae60ac0b8471ab48fbaec41983da05
rogeriodesaf <rogeriodesaf@gmail.com>
2022-04-28 10:31:07 -0300

    resgatando dados individuais com WHERE

---
+++ resgatando_individuais/index.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const exphbs  = require('express-handlebars');
+const mysql   = require('mysql');
+
+const app     = express();
+
+app.engine('handlebars',exphbs.engine())
+app.set('view engine','handlebars')
+
+app.use(express.static('public'))
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+
+const conn = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'',
+    database:'mysql2'
+})
+
+conn.connect(function(err){
+    if(err){
+        console.log(err)
+    }
+    app.listen(4000)
+    console.log('deu certo')
+})
+
+app.get('/',(req,res)=>{
+    res.render('home')
+})
+
+app.get('/animes',(req,res)=>{
+    const sql = "SELECT * FROM books"
+    conn.query(sql,function(err,data){
+        if(err){
+            console.log(err)
+            return
+        }
+        const animes = data
+        res.render('animes',{animes})
+    })
+})
+
+app.post('/animes/insertanimes',(req,res)=>{
+    const title = req.body.title
+    const page  = req.body.page
+
+    const sql = `INSERT INTO books (title,page) VALUES ('${title}','${page}')`
+    conn.query(sql,function(err){
+        if(err){
+            console.log(err)
+        }
+        res.redirect('/animes')
+    })
+})
+
+app.get('/animes/:id',(req,res)=>{
+    const id = req.params.id
+    const sql = `SELECT * FROM books WHERE id = ${id}`
+    conn.query(sql,function(err,data){
+        if(err){
+            console.log(err)
+        }
+        const anime = data[0]
+        res.render('anime',{anime})
+    })
+})
